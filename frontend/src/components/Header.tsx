df29c684eff5017424698e86917e30968507f9aa
Anurax1321 <jyothiranurag@gmail.com>
2025-10-28 14:52:39 -0500

    chnaged the header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
@@ -8,7 +9,13 @@ export default function Header() {
           <h1 className="header-title">Vijayrekha Life Science</h1>
         </div>
         <div className="header-right">
-          {/* Space for future buttons */}
+          <Link to="/" className="home-link">
+            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
+              <path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path>
+              <polyline points="9 22 9 12 15 12 15 22"></polyline>
+            </svg>
+            Home
+          </Link>
         </div>
       </div>
     </header>
