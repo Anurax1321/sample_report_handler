abff11bd804f1497f0aa4f2544600d6c9d42d770
Anurag Chinnaboina <jyothiranurag@gmail.com>
2026-03-06 01:24:34 +0000

    setting up the website in chinnaboina.com wth ngix

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,7 +12,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/vijayrekha">
       <div className="app">
         <Header />
         <Routes>
